5782d16efae71cc6c07ac2ae6b68b8dc3e383b81
TERRY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTE@OOCL.COM>
2019-07-12 08:59:15 +0800

    finish MyMapTest

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -79,7 +79,7 @@ public class Add {
 //    }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter((item)->item%2==0).collect(Collectors.toList());
     }
 
     public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
